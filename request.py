84dd6f2f29b5cd31010633d5357eadc28b1620ab
Dikshanta Chapagain <dikshanta@Dikshantas-MacBook-Air.local>
2025-03-10 19:41:50 +0545

    company name extracted'

--- request.py
+++ request.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://m.timesjobs.com/')
-print(html_text)
+html_text = requests.get('https://m.timesjobs.com/').text
+soup = BeautifulSoup(html_text, "lxml")
+jobs = soup.find_all('li', class_='ui-content search-result')
+
+# Loop through job listings to extract company names
+for job in jobs:
+    company_name = job.find('h4', class_='srp-comp-name')
+    if company_name:
+        print(company_name.prettify())
